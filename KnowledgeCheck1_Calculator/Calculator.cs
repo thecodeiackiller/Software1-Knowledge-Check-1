3ef1f13320eddfbec1f58d2d5d330963794a0733
knunn553 <knunn553@outlook.com>
2024-08-13 18:20:53 -0400

    Refactored Calculator.cs class to implement expression bodied members. I confused EBMs with lambdas but that is clear now. In addition, put the TryParse functionality into a static method within the calculator class and changed the values of the parsed string to floats to enable a real representation of a calculator when dividing. In addition, learned more about the TryParse method and how it evaluates whether or not a value can be parsed into another value AND takes in a reference parameter which we ultimately use in the calculation.

--- KnowledgeCheck1_Calculator/Calculator.cs
+++ KnowledgeCheck1_Calculator/Calculator.cs
@@ -8,24 +8,24 @@ namespace KnowledgeCheck1_Calculator
 {
     public class Calculator
     {
-        public int Add(int first, int second)
+
+        // We are actually going to make this code even simpler with expression-bodied members
+        // Had to look up what EBM's were - though this was lambda expression but its not
+        public static float Add(float a, float b) => a + b;
+        public static float Substract(float a, float b) => (a - b);
+        public static float Multiply(float a , float b) => a * b;
+        public static float Divide(float a , float b) => a / b;
+
+        // Now implementing a method do reduce redundancy and abiding by the DRY principle
+
+        public static bool floatParseCheck(string string1, string string2, out float num1, out float num2)
         {
-            return first + second;
+            if(float.TryParse(string1, out num1) &&  float.TryParse(string2, out num2)) return true;
+            num1 = 0;
+            num2 = 0;
+            return false;
         }
 
-        public int Subtract(int first, int second)
-        {
-            return first - second;
-        }
 
-        public int Multiply(int first, int second)
-        {
-            return first * second;
-        }
-
-        public double Divide(double first, double second)
-        {
-            return first / second;
-        }
     }
 }
--- KnowledgeCheck1_Calculator/Program.cs
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,83 +11,90 @@ namespace KnowledgeCheck1_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to your homemade calculator.");
-            Console.WriteLine("Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division");
+            Console.WriteLine("Let's do some math.");
+            Console.WriteLine("Enter 1 to Add");
+            Console.WriteLine("Enter 2 to Subtract");
+            Console.WriteLine("Enter 3 to Multiply");
+            Console.WriteLine("Enter 4 to Divide");
+            Console.WriteLine();
 
-            var input = Console.ReadLine();
-            var calculator = new Calculator();
+            string mathStart = "Enter two numbers. Press ENTER after each number.";
+            string errorMessage = "Invalid numbers. Please enter two valid numbers";
+            string calculation = Console.ReadLine();
 
-            switch (input)
+            switch(calculation)
             {
                 case "1":
-                    Console.WriteLine("Enter 2 integers to add");
-                    var addNumber1 = Console.ReadLine();
-                    var addNumber2 = Console.ReadLine();
-
-                    if (int.TryParse(addNumber1, out int addNumOne) && int.TryParse(addNumber2, out int addNumTwo))
+                    Console.WriteLine(mathStart);
+                    // Is there a way to write a method that can simplify things even more when getting the input for the user:
+                    string userInput1 = Console.ReadLine();
+                    string userInput2 = Console.ReadLine();
+                    if(Calculator.floatParseCheck(userInput1, userInput2, out float num1,out float num2)) 
                     {
-                        Console.Write($"{addNumber1} + {addNumber2} = ");
-                        Console.Write(calculator.Add(addNumOne, addNumTwo));
+
+                        Console.WriteLine($"{num1} + {num2} = {Calculator.Add(num1, num2)}");
+
+
                     }
                     else
                     {
-                        Console.WriteLine("One or more of the numbers is not an int");
+                        Console.WriteLine(errorMessage);
                     }
-                    break;
-
-                case "2":
-                    Console.WriteLine("Enter 2 integers to subtract");
-                    var subtractNumber1 = Console.ReadLine();
-                    var subtractNumber2 = Console.ReadLine();
-
-                    if (int.TryParse(subtractNumber1, out int subNumOne) && int.TryParse(subtractNumber2, out int subNumTwo))
-                    {
-                        Console.Write($"{subtractNumber1} - {subtractNumber2} = ");
-                        Console.Write(calculator.Subtract(subNumOne, subNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
-                    }
-                    break;
-
-                case "3":
-                    Console.WriteLine("Enter 2 integers to multiply");
-                    var multiplynum1 = Console.ReadLine();
-                    var multiplynum2 = Console.ReadLine();
-
-                    if(int.TryParse(multiplynum1,out int mulNumOne) && int.TryParse(multiplynum2 ,out int mulNumTwo))
-                    {
-                        Console.Write($"Multiplying numbers {multiplynum1} & {multiplynum2} equals");
-                        Console.Write(calculator.Multiply(mulNumTwo,mulNumOne));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One of more of the integers is not an int.");
-                    }
-                    break;
-
-                case "4":
-                    Console.WriteLine("Enter 2 integers to divide");
-                    var divideNumber1 = Console.ReadLine();
-                    var divideNumber2 = Console.ReadLine();
-
-                    if (double.TryParse(divideNumber1, out double divNumOne) && double.TryParse(divideNumber2, out double divNumTwo))
-                    {
-                        Console.Write($"{divideNumber1} / {divideNumber2} = ");
-                        Console.Write(calculator.Divide(divNumOne, divNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
-                    }
-                    break;
-                    break;
-
-                default:
-                    Console.WriteLine("Unknown input. Please enter a number 1 through 4.");
                     break;
             }
+            switch(calculation)
+            {
+                case "2":
+                    Console.WriteLine(mathStart);
+                    string userInput1 = Console.ReadLine();
+                    string userInput2 = Console.ReadLine();
+                    if(Calculator.floatParseCheck(userInput1,userInput2, out float num1, out float num2))
+                    {
+                        Console.WriteLine($"{num1} - {num2} = {Calculator.Substract(num1,num2)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine(errorMessage);
+                    }
+                    break;
+            }
+            switch (calculation)
+            {
+                case "3":
+                    Console.WriteLine(mathStart);
+                    string userInput1 = Console.ReadLine();
+                    string userInput2 = Console.ReadLine();
+                    if (Calculator.floatParseCheck(userInput1, userInput2, out float num1, out float num2))
+                    {
+                        Console.WriteLine($"{num1} * {num2} = {Calculator.Multiply(num1, num2)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine(errorMessage);
+                    }
+                    
+                    break;
+            }
+            switch (calculation)
+            {
+                case "4":
+                    Console.WriteLine(mathStart);
+                    string userInput1 = Console.ReadLine();
+                    string userInput2 = Console.ReadLine();
+                    if (Calculator.floatParseCheck(userInput1, userInput2, out float num1, out float num2))
+                    {
+                        Console.WriteLine($"{num1} / {num2} = {Calculator.Divide(num1, num2)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine(errorMessage);
+                    }
+                    break;
+            }
+
+
+
+
         }
     }
 }
